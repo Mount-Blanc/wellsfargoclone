16bf4cd9af1b2face7394fceb886a6b0518e6d7c
Mount-Blanc <mountblanc100@gmail.com>
2023-07-10 09:54:09 -0700

    added category and options section

--- wellsfargo/src/App.jsx
+++ wellsfargo/src/App.jsx
@@ -7,17 +7,48 @@ function App() {
     <>
     <a href='#main'></a>
      <header>
+      <div className='header-container'>
       <div>logo</div>
-      <div>
-        <ul>
+      <nav className='nav-container'>
+        <ul className='list'>
           <li> <a href=''>ATMs/Locations</a></li>
           <li> <a href=''>Help</a></li>
           <li> <a href=''>About Us</a></li>
           <li> <a href=''>Espanol</a></li>
 
         </ul>
+      </nav>
       </div>
      </header>
+     <div>
+      <div className='category-container'>
+      <ul className='category-list'>
+        <li><a href=''>Personal</a></li>
+        <li><a href=''>Investing & Wealth Management</a></li>
+        <li><a href=''>Small Business</a></li>
+        <li><a href=''>Commercial Banking</a></li>
+        <li><a href=''>Corporate & Investment Banking</a></li>
+
+      </ul>
+     </div>
+     </div>
+     <div>
+      <div className='options-container'>
+      <ul className='options-list'>
+        <li><a href=''>Checking</a></li>
+        <li><a href=''>Credit Cards</a></li>
+        <li><a href=''>Home Loans</a></li>
+        <li><a href=''>Personal Loans</a></li>
+        <li><a href=''>Auto Loans</a></li>
+        <li><a href=''>Premier</a></li>
+        <li><a href=''>Education & Tools</a></li>
+
+
+
+
+      </ul>
+     </div>
+     </div>
     </>
   )
 }
